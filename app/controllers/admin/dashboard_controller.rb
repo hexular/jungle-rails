d34286db6cae9490c3b10ac214a894b43480ac69
Daniel Braverman <hexular@gmail.com>
2019-12-10 21:36:48 +0000

    add admin dashboard page display

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class Admin::DashboardController < ApplicationController
   
-  http_basic_authenticate_with name: ENV['ADMIN_AUTH_NAME'], password: ENV['ADMIN_AUTH_PASSWORD']
+  http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
+    @products = Product.count(:id)
+    @categories = Category.count(:id)
   end
+
 end
